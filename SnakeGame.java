988a80353699df5d1499f9d818248ef8c4e96c49
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-12-20 16:37:55 +0100

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,34 +1,38 @@
 package com.codegym.games.snake;
-
 import com.codegym.engine.cell.*;
 
 public class SnakeGame extends Game {
-    public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    public static final int WIDTH = 15;
     private Snake snake;
-
+    private int turnDelay;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
-
-
-
     }
 
-    private void createGame() {
-        Snake snaky = new Snake(WIDTH/2, HEIGHT/2);
-        this.snake = snaky;
+    @Override
+    public void onTurn(int sec) {
+        snake.move();
         drawScene();
     }
+    private void createGame() {
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
+        Snake snake = new Snake(WIDTH/2, HEIGHT/2);
+        this.snake = snake;
+        drawScene();
+
+    }
+
     private void drawScene() {
-        for(int i = 0; i < WIDTH; i++) {
-            for(int j = 0; j < HEIGHT; j++) {
-                setCellColor(i, j, Color.DARKSEAGREEN);
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                setCellValueEx(x,y,Color.DARKSEAGREEN, "");
             }
         }
         snake.draw(this);
     }
-
 }
