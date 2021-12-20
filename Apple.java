566497959621e00989b0aeba1dc3d8a92a118c48
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-12-20 16:36:39 +0100

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -3,13 +3,12 @@ import com.codegym.engine.cell.*;
 
 public class Apple extends GameObject {
     private static final String APPLE_SIGN = "\uD83C\uDF4E";
+    public boolean isAlive = true;
 
     public Apple(int x, int y) {
-        super(x, y);
+        super(x,y);
     }
-
     public void draw(Game game) {
-        game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.WHITE, 75);
+        game.setCellValueEx(x, y, Color.NONE, APPLE_SIGN, Color.GREEN, 75);
     }
 }
-
