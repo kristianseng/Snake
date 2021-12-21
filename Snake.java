f8749821991ddb650f95f98d53698d1c4d7f2cd9
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-12-21 23:54:44 +0100

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,76 +1,77 @@
 package com.codegym.games.snake;
+
 import com.codegym.engine.cell.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
-    public boolean isAlive = true;
 
-    private Direction direction = Direction.LEFT;
-    private static final String HEAD_SIGN  = "\uD83D\uDC7E";
-    private static final String BODY_SIGN  = "\u26AB";
-
+    private final static String HEAD_SIGN = "\uD83D\uDC7E";
+    private final static String BODY_SIGN = "\u26AB";
 
     private List<GameObject> snakeParts = new ArrayList<>();
-
+    public boolean isAlive = true;
+    private Direction direction = Direction.LEFT;
 
     public Snake(int x, int y) {
-        snakeParts.add(new GameObject(x,y));
-        snakeParts.add(new GameObject(x+1,y));
-        snakeParts.add(new GameObject(x+2,y));
+        snakeParts.add(new GameObject(x, y));
+        snakeParts.add(new GameObject(x + 1, y));
+        snakeParts.add(new GameObject(x + 2, y));
     }
 
-    public void move() {
-        GameObject obj = createNewHead();
-        if(obj.x >= SnakeGame.WIDTH || obj.x < 0 || obj.y >= SnakeGame.WIDTH || obj.y < 0) {
-            isAlive = false;
-        } else {
-            snakeParts.add(0, obj);
-            removeTail();
+    public void draw(Game game) {
+        Color color = isAlive ? Color.BLACK : Color.RED;
+
+        for (int i = 0; i < snakeParts.size(); i++) {
+            GameObject part = snakeParts.get(i);
+            String sign = (i != 0) ? BODY_SIGN : HEAD_SIGN;
+            game.setCellValueEx(part.x, part.y, Color.NONE, sign, color, 75);
         }
     }
 
+    public void move(Apple apple) {
+        GameObject newHead = createNewHead();
+        if (newHead.x >= SnakeGame.WIDTH
+                || newHead.x < 0
+                || newHead.y >= SnakeGame.HEIGHT
+                || newHead.y < 0) {
+            isAlive = false;
+            return;
+        }
+        if(newHead.x == apple.x && newHead.y == apple.y) {
+            apple.isAlive = false;
+            snakeParts.add(0, newHead);
+            return;
+        }
+        snakeParts.add(0, newHead);
+        removeTail();
+    }
+
     public GameObject createNewHead() {
-        GameObject obj = snakeParts.get(0);
-        switch (direction) {
-            case LEFT:
-                return new GameObject(obj.x - 1, obj.y);
-            case RIGHT:
-                return new GameObject(obj.x + 1, obj.y);
-            case UP:
-                return new GameObject(obj.x, obj.y - 1);
-            case DOWN:
-                return new GameObject(obj.x, obj.y + 1);
-            default:
-                throw new IllegalStateException("Unexpected value: " + direction);
+        GameObject oldHead = snakeParts.get(0);
+        if (direction == Direction.LEFT) {
+            return new GameObject(oldHead.x - 1, oldHead.y);
+        } else if (direction == Direction.RIGHT) {
+            return new GameObject(oldHead.x + 1, oldHead.y);
+        } else if (direction == Direction.UP) {
+            return new GameObject(oldHead.x, oldHead.y - 1);
+        } else {
+            return new GameObject(oldHead.x, oldHead.y + 1);
         }
     }
 
     public void removeTail() {
-        snakeParts.remove(snakeParts.size()-1);
-    }
-
-    public void draw(Game game) {
-        for(GameObject obj : snakeParts) {
-            if(snakeParts.get(0).equals(obj)) {
-                if(isAlive) {
-                    game.setCellValueEx(obj.x, obj.y, Color.NONE, HEAD_SIGN, Color.BLACK, 75);
-                } else {
-                    game.setCellValueEx(obj.x, obj.y, Color.NONE, HEAD_SIGN, Color.RED, 75);
-                }
-            } else {
-                if(isAlive) {
-                    game.setCellValueEx(obj.x, obj.y, Color.NONE, BODY_SIGN, Color.BLACK, 75);
-                } else {
-                    game.setCellValueEx(obj.x, obj.y, Color.NONE, BODY_SIGN, Color.RED, 75);
-                };
-            }
-
-        }
+        snakeParts.remove(snakeParts.size() - 1);
     }
 
     public void setDirection(Direction direction) {
+        if ((direction == Direction.UP && this.direction == Direction.DOWN)
+                || (direction == Direction.LEFT && this.direction == Direction.RIGHT)
+                || (direction == Direction.RIGHT && this.direction == Direction.LEFT)
+                || (direction == Direction.DOWN && this.direction == Direction.UP))
+            return;
+
         this.direction = direction;
     }
 }
