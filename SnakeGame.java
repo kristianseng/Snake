390b44d2c4aaadf425791c14fc275a5e4e637418
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-12-23 23:51:40 +0100

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,10 +1,15 @@
 package com.codegym.games.snake;
+
 import com.codegym.engine.cell.*;
 
 public class SnakeGame extends Game {
-    public static final int HEIGHT = 15;
+
     public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
     private Snake snake;
+    private Apple apple;
+
     private int turnDelay;
 
     @Override
@@ -13,26 +18,40 @@ public class SnakeGame extends Game {
         createGame();
     }
 
-    @Override
-    public void onTurn(int sec) {
-        snake.move();
-        drawScene();
-    }
     private void createGame() {
         turnDelay = 300;
         setTurnTimer(turnDelay);
-        Snake snake = new Snake(WIDTH/2, HEIGHT/2);
-        this.snake = snake;
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
+        apple = new Apple(5,5);
         drawScene();
-
     }
 
     private void drawScene() {
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                setCellValueEx(x,y,Color.DARKSEAGREEN, "");
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellValueEx(i, j, Color.DARKSEAGREEN, "");
             }
         }
         snake.draw(this);
+        apple.draw(this);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        snake.move(apple);
+        drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        if (key == Key.LEFT) {
+            snake.setDirection(Direction.LEFT);
+        } else if (key == Key.RIGHT) {
+            snake.setDirection(Direction.RIGHT);
+        } else if (key == Key.UP) {
+            snake.setDirection(Direction.UP);
+        } else if (key == Key.DOWN) {
+            snake.setDirection(Direction.DOWN);
+        }
     }
 }
